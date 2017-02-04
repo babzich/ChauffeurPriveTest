e15caa28fd4e2363ecba65b120dd20da94524c41
Vincent <contact@vincent-bach.fr>
2017-02-04 11:14:44 +0100

    Cleanup project

---
+++ ChauffeurPrive/MapViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MapViewController.swift
+//  ChauffeurPrive
+//
+//  Created by Vincent Bach on 04/02/2017.
+//  Copyright © 2017 Vincent Bach. All rights reserved.
+//
+
+import UIKit
+
+final class MapViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
